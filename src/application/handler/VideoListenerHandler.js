6605379dcda0d47ee74f72a25049787df93bdb96
Feny <1220301855@qq.com>
2025-03-02 20:23:34 +0800

    优化播放结束退出网页全屏的代码。

--- src/application/handler/VideoListenerHandler.js
+++ src/application/handler/VideoListenerHandler.js
@@ -31,7 +31,7 @@ export default {
     if (!BILI_VID_REG.test(href) && !ACFUN_VID_REG.test(href)) return;
     // B站视频合集播放的是合集最后一个或关闭了合集自动连播
     const pods = App.querys('.video-pod .switch-btn:not(.on), .video-pod__item:last-of-type[data-scrolled="true"]');
-    if (pods.length === 0) return;
+    if (pods.length > 0) return App.exitWebFullScreen();
     App.exitWebFullScreen();
   },
 };
